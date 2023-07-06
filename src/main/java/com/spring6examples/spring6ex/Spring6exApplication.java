b7d6c049c3d345c02b644ba1fd2fb77e344e9490
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-07-06 10:49:38 -0400

    Initial commit

---
+++ src/main/java/com/spring6examples/spring6ex/Spring6exApplication.java
@@ -0,0 +1,13 @@
+package com.spring6examples.spring6ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6exApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring6examples/spring6ex/Spring6exApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring6examples.spring6ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6exApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
