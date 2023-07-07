e9aaa307af67bddfcbbda5c0e695062859515752
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-07-07 16:01:15 -0400

    added default to profile to make to first profile spring uses

--- src/main/java/com/spring6examples/spring6ex/services/i18n/EnglishGreetingService.java
+++ src/main/java/com/spring6examples/spring6ex/services/i18n/EnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.spring6examples.spring6ex.services.i18n;
 import com.spring6examples.spring6ex.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class EnglishGreetingService implements GreetingService {
     @Override
